4cc3596a052a6291904a47e6b0a561af40d9ea29
안덕우 <fjrlvkdlxj@naver.com>
2022-11-22 00:56:15 +0900

    2022.11.22

--- navigation/app/src/main/java/com/example/navigation/MainActivity.java
+++ navigation/app/src/main/java/com/example/navigation/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import com.example.navigation.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setupJetpackNavigation();
     }
+
+    private void setupJetpackNavigation() {
+        NavHostFragment host = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        navController = host.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.search, R.id.like).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
 }
